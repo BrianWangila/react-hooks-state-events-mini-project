34e5d487ad5310fbabb20ad1c3a6390d27a217ab
Brian Wangila <bwanjalah@gmail.com>
2022-07-25 15:11:24 +0300

    Done.

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+// import { v4 as uuid } from "uuid";
+ 
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit}) {
+
+  const [task, setTask] = useState("")
+  const [category, setCategory] = useState("All")
+
+  function handleTaskSubmit(e){
+    e.preventDefault()
+
+
+    const newTask ={
+      text: task,
+      category: category
+    }
+    onTaskFormSubmit(newTask)
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleTaskSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={task} onChange={(e)=> setTask(e.target.value)}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" value={category} onChange={(e) => setCategory(e.target.value)}>
           {/* render <option> elements for each category here */}
+          <option value="All">All</option>
+          <option value="Food">Food</option>
+          <option value="Money">Money</option>
+          <option value="Code">Code</option>
+          <option value="Misc">Misc</option>
+
         </select>
       </label>
       <input type="submit" value="Add task" />
